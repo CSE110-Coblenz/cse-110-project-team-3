0e3207efcce700bd8dabe8830237819732256a8f
neodimaya <158006532+neodimaya@users.noreply.github.com>
2025-10-28 20:00:51 -0700

    Added skeleton script for reference screens
    reference screen controller is not working yet.

---
+++ screens/ReferenceScreens/ReferenceScreenController.ts
@@ -0,0 +1,34 @@
+
+import { ScreenContoller } from "../../types.ts";
+import type { ScreenSwitcher} from "../../types.ts";
+import { ReferenceScreenView } from "./ReferenceScreen.ts";
+
+
+/*
+ReferenceController handles the reference screen interactions
+*/
+
+export class ReferenceScreenController extends ScreenController {
+    private view: ReferenceScreenView;
+    private screenSwitcher: ScreenSwitcher;
+
+    constructor(screenSwitcher: ScreenSwitcher) {
+        super();
+        this.screenSwitcher = screenSwitcher;
+        this.view = new ReferenceScreenView(() => this.handleExitClick());
+    }
+
+    /*
+    Handles the exit button click to be implemented after game view is implemented?
+    
+    private handleExitClick(): void {
+        this.screenSwitcher.switchToScreen({type: "game"});
+    }
+    */
+    /*
+    Gets the reference screen view
+    */
+    getView(): ReferenceScreenView {
+        return this.view;
+    }
+}
