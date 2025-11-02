25c881e946160023660b55591131c6a8533ed0cc
neodimaya <158006532+neodimaya@users.noreply.github.com>
2025-11-02 07:02:47 -0800

    commented out the reference screen

--- src/main.ts
+++ src/main.ts
@@ -61,8 +61,8 @@ class App implements ScreenSwitcher {
     //this.layer.add(this.mapScreenController.getView().getGroup());
     this.layer.add(this.referenceScreenController.getView().getGroup());
     // Start with the map screen
-    //this.mapScreenController.getView().show();
-    this.referenceScreenController.getView().show();
+    this.mapScreenController.getView().show();
+    //this.referenceScreenController.getView().show();
   }
 
   switchToScreen(screen: Screen): void {
@@ -88,6 +88,13 @@ class App implements ScreenSwitcher {
       // Add cases for other screens as needed
       case "reference":
         this.referenceScreenController.getView().show();
+        break;
+      case "topic":
+        if (screen.level === "friction") {
+          this.frictionTopicController.getView().show();
+        } else if (screen.level === "projectile motion") {
+          this.projectileMotionTopicController.getView().show();
+        }
         break;
       case "topic":
         if (screen.level === "friction") {
