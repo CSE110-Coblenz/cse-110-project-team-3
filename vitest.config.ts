d1a7a6d75ee32dbd23eedb83264133440c50dd90
7174Andy <hep003@ucsd.edu>
2025-11-12 19:31:58 -0800

    simplify vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: "jsdom", // so DOM APIs exist (window, document, etc.)
-    globals: true, // allows `describe`, `it`, `expect` without imports
-    include: ["tests/**/*.test.ts", "tests/**/*.spec.ts"], // adjust to your folder
-    coverage: {
-      reporter: ["text", "html"],
-    },
+    environment: "jsdom",
+    globals: true,
   },
 });
