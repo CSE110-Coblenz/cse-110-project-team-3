33c9001b898e01df935a75d44439394ed387fad0
Ishita Chawla <chawlaishita13@gmail.com>
2025-11-26 11:30:51 -0800

    Small fix in friction simulation

--- src/configs/simulations/lev2-friction.ts
+++ src/configs/simulations/lev2-friction.ts
@@ -6,7 +6,7 @@ export const Lev2SimulationConfig: SimulationScreenConfig = {
   description: `
 A 5.0 kg crate rests on a rough horizontal surface with a coefficient of static friction μs = 0.4 and kinetic friction μk = 0.3. 
 You apply a horizontal force to the crate.
-What is the minimum force required to start moving the crate?
+What is the minimum force required to start moving the crate (g=9.8m/s²)?
 `,
 
   picture: {
